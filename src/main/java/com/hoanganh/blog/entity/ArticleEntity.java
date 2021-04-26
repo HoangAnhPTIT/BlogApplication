207a590899fd2cfe68526e9e3ee60954dd65809e
HoangAnhPTIT <caohoanganh2000@gmail.com>
2021-04-26 22:52:54 +0700

    apply jwt

---
+++ src/main/java/com/hoanganh/blog/entity/ArticleEntity.java
@@ -0,0 +1,5 @@
+package com.hoanganh.blog.entity;
+
+public class ArticleEntity {
+
+}
---
+++ src/main/java/com/hoanganh/blog/entity/UserEntity.java
@@ -0,0 +1,17 @@
+package com.hoanganh.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+}
